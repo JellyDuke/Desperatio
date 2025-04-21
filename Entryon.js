9848614fcbd534c97a2f7f6938c70636754b82e9
ParkLouis <73382228+JellyDuke@users.noreply.github.com>
2025-04-21 17:37:57 +0900

    Update Entryon.js

--- Entryon.js
+++ Entryon.js
@@ -1,1 +1,6 @@
-
+fetch('https://twelvedata-proxy.vercel.app/api/twelvedata?symbol=AAPL&indicator=rsi')
+  .then(res => res.json())
+  .then(data => {
+    console.log("RSI:", data.value);  // 예: 63.45
+    // 여기에 매수/매도 판단 넣을 수 있음
+  });
